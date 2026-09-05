c0add71c44856f9686416f2bd3a71ac97caab5a5
Najiib <najiibqoobey290@gmail.com>
2026-09-05 13:56:36 +0300

    Complete Book and Coffee classes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    # Initialization of a Book object
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # Ensuring the page_count is an integer using property
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if type(value) is int:
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    # Turning a page
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
-        
+# Creating a Book instance
+book1 = Book("Words from wises", 133)
+
+print(book1.title)
+print(book1.page_count)
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    # Initialization of Coffee instance
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    # Ensuring the size of the coffee as property
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value == "Small" or value == "Medium" or value == "Large":
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    # Adding 1 to the price as a tip
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
+
+# Creating new instance
+coffee1 = Coffee("Large", 6)
+
+print(coffee1.size)
+print(coffee1.price)
+
+coffee1.tip()
+print(coffee1.price)
